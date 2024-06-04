b150f17049363132bc7d098244b9f45f9652cc23
Marius Austerschulte <m.austerschulte@conterra.de>
2024-06-04 10:15:21 +0200

    Added StepFactory.ts and Tour.ts to new bundle dn_intro

---
+++ src/main/js/bundles/dn_intro/Tour.ts
@@ -0,0 +1,43 @@
+///
+/// Copyright (C) 2023 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
+import {driver} from "driver";
+import StepFactory, {MapappsToolDriveStep} from "./StepFactory";
+import {InjectedReference} from "apprt-core/InjectedReference";
+
+export default class Tour {
+    private stepDefinitions: MapappsToolDriveStep[] = [];
+    private stepFactory: InjectedReference<StepFactory>;
+
+    constructor(properties: ConstructorParameters) {
+        this.stepDefinitions = properties.steps;
+    }
+
+    startTour(): void {
+        const driverObj= driver.driver({
+            showProgress: true,
+            disableActiveInteraction: true // This avoids the tour from getting out of sync with the app due to user interactions on the tools.
+        });
+        const stepDefinitions = this.stepDefinitions;
+        const steps = stepDefinitions.map(stepDefinition => this.stepFactory!.createStep(driverObj, stepDefinition));
+        driverObj.setSteps(steps);
+        driverObj.drive();
+    }
+}
+
+interface ConstructorParameters {
+    steps: MapappsToolDriveStep[]
+}
--- src/main/js/bundles/dn_intro/module.ts
+++ src/main/js/bundles/dn_intro/module.ts
@@ -14,3 +14,5 @@
 /// limitations under the License.
 ///
 
+export {default as StepFactory} from "./StepFactory";
+export {default as Tour} from "./Tour";
